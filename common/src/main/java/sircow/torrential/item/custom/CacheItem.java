fbaa8840eaccb607c11ce13b09959c975bed158e
Sir-Cow <16866289+Sir-Cow@users.noreply.github.com>
2026-08-30 14:59:02 +0100

    1.0.2
    - make cache drop item contents if destroyed

--- common/src/main/java/sircow/torrential/item/custom/CacheItem.java
+++ common/src/main/java/sircow/torrential/item/custom/CacheItem.java
@@ -1,9 +1,13 @@
 package sircow.torrential.item.custom;
 
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemUtils;
+import net.minecraft.world.item.component.ItemContainerContents;
 import net.minecraft.world.level.Level;
 import org.jspecify.annotations.NonNull;
 
@@ -27,5 +31,14 @@ public class CacheItem extends Item {
     @Override
     public boolean canFitInsideContainerItems() {
         return false;
+    }
+
+    @Override
+    public void onDestroyed(final ItemEntity entity) {
+        ItemContainerContents contents = entity.getItem().get(DataComponents.CONTAINER);
+        if (contents != null) {
+            entity.getItem().set(DataComponents.CONTAINER, ItemContainerContents.EMPTY);
+            ItemUtils.onContainerDestroyed(entity, contents.allItemsCopyStream());
+        }
     }
 }
